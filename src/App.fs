780109104152f5bf61214d2d72bdb0b03791bd34
Unknown <vypon3000@gmail.com>
2019-01-31 00:53:52 -0500

    Added some events, processing, styles

--- src/App.fs
+++ src/App.fs
@@ -5,9 +5,10 @@ open Fable.Core
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 open Fable.Import
-open System
 open Fable.PowerPack
 open Fable.PowerPack.Fetch
+open System
+open System.Text.RegularExpressions
 // MODEL
 type DictEntry = {
                   literal : string
@@ -22,6 +23,7 @@ type Model = {
               prompt : string
               answer : string
               input : string
+              valid : bool
               dict : Dictionary
               }
 
@@ -30,12 +32,13 @@ type Msg =
  | FailDictionary of exn
  | LoadDictionary of Dictionary
  | FetchNewQuery
- | TryAnswer of string
- | FormatAnswer of string
+ | TryAnswer
+ | SetInputValue of string
+ | CheckValid
 
-let init() = { prompt = ""; answer = ""; input = ""; dict = { size = 0; kanji = [||] } }, Cmd.ofMsg FetchDictionary
-let fetchDictionary () : JS.Promise<Dictionary> =
-    promise{
+let init() = { prompt = ""; answer = ""; input = ""; valid = false; dict = { size = 0; kanji = [||] } }, Cmd.ofMsg FetchDictionary
+let fetchDictionary() : JS.Promise<Dictionary> =
+    promise {
         let! res = fetch "/dict.json" []
         return! res.json<Dictionary>()
     }
@@ -52,19 +55,31 @@ let rec update (msg : Msg) (model : Model) =
     | FetchNewQuery ->
             let rand : DictEntry = model.dict.kanji.[Random().Next(model.dict.size)]
             { model with prompt = rand.literal; answer = rand.skipCode; input = "" }, Cmd.none
-    | TryAnswer s -> model, Cmd.none
-    | FormatAnswer s -> model, Cmd.none
+    | TryAnswer -> model, Cmd.none
+    | SetInputValue s -> { model with input = s }, Cmd.ofMsg CheckValid
+    | CheckValid ->
+        if Regex.Match(model.input, "[1-4]{1}[-]{1}[1-9]+[0-9]*[-]{1}[1-9]+[0-9]*").Success then
+            { model with valid = true }, Cmd.none
+        else
+            model, Cmd.none
 
 // VIEW (rendered with React)
 
 let view (model : Model) dispatch =
-    div []
+    div [ Class "container" ]
       [
-        h2 [] [ str model.prompt ]
-        br []
-        input []
-        br []
-        button [ OnClick(fun _ -> dispatch (TryAnswer model.input)) ] [ str "Answer" ]
+        div [ Class "prompt-box" ] [
+                h2 [ Id "kanji-prompt" ] [ str model.prompt ]
+                br []
+                input [ Id "skip-code-answer"
+                        Typeof "text"
+                        Pattern "[1-4]{1}[-]{1}[1-9]+[0-9]*[-]{1}[1-9]+[0-9]*"
+                        OnChange(fun ev -> ev.Value |> SetInputValue |> dispatch)
+                       ]
+                br []
+                button [ Id "skip-code-submit"; OnClick(fun _ -> dispatch (TryAnswer)) ] [ str "Answer" ]
+                button [ Id "skip-code-next"; OnClick(fun _ -> dispatch FetchNewQuery) ] [ str "Next" ]
+        ]
       ]
 
 // App
